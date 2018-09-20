d7b0071635ec9f24a3769262942733c232dc124d
Skylark <37351525+5kyl4rk@users.noreply.github.com>
2018-09-20 12:33:48 -0600

    made Getters and Setters for the Piano model

--- src/controller/IfController.java
+++ src/controller/IfController.java
@@ -1,9 +1,12 @@
 package controller;
 
+import model.Piano;
+import java.util.Scanner;
+
 public class IfController
 {
 	public void start()
 	{
-
+		
 	}
 }
--- src/model/Piano.java
+++ src/model/Piano.java
@@ -2,9 +2,99 @@ package model;
 
 public class Piano
 {
+	// Declaration section
+	// Data Member section
+	/*
+	 * Also called instance variables these are special - they get a "default" value
+	 * All numbers -> 0 boolean -> false Objects -> null
+	 */
 	private String brand;
 	private String color;
 	private boolean isElectric;
+	private boolean isGrand;
 	private int numberOfKeys;
+
+	/**
+	 * Default constructor for a piano Initializes all variables to valid but "bad"
+	 * values Used for later customization
+	 */
+	public Piano()
+	{
+		this.brand = "Nintendo Labo: Cardboard Piano edition (null)";
+		this.color = "Expensive cardboard (null)";
+		this.numberOfKeys = -88;
+	}
+
+	/**
+	 * Initializes a Piano instance
+	 * 
+	 * @param brand
+	 *            The type of piano
+	 * @param color
+	 *            The color or make of the piano
+	 * @param isElectric
+	 *            If it's electric or uses electric elements
+	 * @param isGrand
+	 *            If it's a grand piano
+	 * @param numberOfKeys
+	 *            The number of keys it has
+	 */
+	public Piano(String brand, String color, boolean isElectric, boolean isGrand, int numberOfKeys)
+	{
+		this.brand = brand;
+		this.color = color;
+		this.isElectric = isElectric;
+		this.isGrand = isGrand;
+		this.numberOfKeys = numberOfKeys;
+	}
+
+	public String getBrand()
+	{
+		return brand;
+	}
+
+	public String getColor()
+	{
+		return color;
+	}
+
+	public boolean getIsElectric()
+	{
+		return isElectric;
+	}
+
+	public boolean getIsGrand()
+	{
+		return isGrand;
+	}
+
+	public int getNumberOfKeys()
+	{
+		return numberOfKeys;
+	}
+
+	public void setBrand(String brand)
+	{
+		this.brand = brand;
+	}
 	
+	public void setColor(String color)
+	{
+		this.color = color;
+	}
+	
+	public void setIsElectric(boolean electric)
+	{
+		isElectric = electric;
+	}
+	
+	public void setIsGrand(boolean grand)
+	{
+		isGrand = grand;
+	}
+	
+	public void setNumberOfKeys(int keys)
+	{
+		numberOfKeys = keys;
+	}
 }
