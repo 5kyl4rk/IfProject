f9fc9361e68a7ea6ba3ca4a2a4b239302be5acf3
Skylark <37351525+5kyl4rk@users.noreply.github.com>
2018-09-20 13:07:43 -0600

    started adding in input [WIP ]
    Need to add toString() method and validation for boolean input

--- src/controller/IfController.java
+++ src/controller/IfController.java
@@ -1,10 +1,20 @@
 package controller;
 
 import model.Piano;
-import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class IfController
 {
+	/**
+	 * Builds the instance of the Controller.
+	 */
+	public IfController() 
+	{
+		
+	}
+	/**
+	 * This is where the program starts calling methods to run the program
+	 */
 	public void start()
 	{
 		
