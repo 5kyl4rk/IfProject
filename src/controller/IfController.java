d1ea4d271b6627ee897f21b213b507f9cf46e3c2
Skylark <37351525+5kyl4rk@users.noreply.github.com>
2018-09-24 12:39:05 -0600

    added in int validation and input validation for boolean

--- src/controller/IfController.java
+++ src/controller/IfController.java
@@ -5,18 +5,65 @@ import javax.swing.JOptionPane;
 
 public class IfController
 {
+	private Piano userPiano;
+
 	/**
 	 * Builds the instance of the Controller.
 	 */
-	public IfController() 
+	public IfController()
 	{
-		
+
 	}
+
 	/**
 	 * This is where the program starts calling methods to run the program
 	 */
 	public void start()
 	{
-		
+		String userInput = JOptionPane.showInputDialog(null, "What model is your piano?");
+		userPiano.setBrand(userInput);
+
+		userInput = JOptionPane.showInputDialog(null, "What color is it?");
+		userPiano.setColor(userInput);
+
+		userInput = JOptionPane.showInputDialog(null, "Does it use electricity to power it?");
+		boolean approved = false;
+		while (!approved)
+		{
+			if (userInput.equalsIgnoreCase("yes") || userInput.equalsIgnoreCase("true"))
+			{
+				approved = true;
+				userPiano.setIsElectric(true);
+				JOptionPane.showMessageDialog(null, "Your piano needs power!");
+				JOptionPane.showMessageDialog(null, "better have an outlet handy");
+				
+			}else if(userInput.equalsIgnoreCase("no") || userInput.equalsIgnoreCase("false"))
+			{
+				approved = true;
+				userPiano.setIsElectric(false);
+				JOptionPane.showMessageDialog(null, "classic");
+				
+			} else 
+			{
+				JOptionPane.showMessageDialog(null, "Please answer yes/no or true/false");
+			}
+		}
+
+	}
+
+	public boolean validInt(String maybeInt)
+	{
+		boolean isValid = false;
+
+		try
+		{
+			Integer.parseInt(maybeInt);
+			isValid = true;
+		}
+		catch (NumberFormatException nope)
+		{
+			JOptionPane.showMessageDialog(null, "Please enter an integer");
+		}
+		return isValid;
 	}
 }
